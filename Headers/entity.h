1180609351a01778dc9eebfb78fdd60bae796c46
Maanav Rajesh <maanavrajesh13@gmail.com>
2021-12-01 15:20:31 -0500

    enemy movement

--- Headers/entity.h
+++ Headers/entity.h
@@ -3,28 +3,28 @@
 
 #include "utility.h"
 
-class Entity
-{
+class Entity {
 public:
-    Entity();
-    virtual ~Entity() =0;
+  Entity();
+  virtual ~Entity() = 0;
 
-    virtual int hp() { return m_hp; }
-    virtual int atk() { return m_atk; }
-    virtual int def() { return m_def; }
-    void setHp(int hp) { m_hp = hp; }
-    void setAtk(int atk) { m_atk = atk; }
-    void setDef(int def) { m_def = def; }
+  virtual int hp() { return m_hp; }
+  virtual int atk() { return m_atk; }
+  virtual int def() { return m_def; }
+  void setHp(int hp) { m_hp = hp; }
+  void setAtk(int atk) { m_atk = atk; }
+  void setDef(int def) { m_def = def; }
 
-    Utility::Direction move();
-    virtual int defend(int atk);
-    virtual int attack();
-    virtual Utility::Effect interacted();
-    virtual Utility::Effect collected();
-    virtual bool permeable();
-    virtual char draw();
+  Utility::Direction move();
+  virtual int defend(int atk);
+  virtual int attack(Entity *defender);
+  virtual Utility::Effect interacted();
+  virtual Utility::Effect collected();
+  virtual bool permeable();
+  virtual char draw();
+
 private:
-    int m_hp, m_atk, m_def;
+  int m_hp, m_atk, m_def;
 };
 
 #endif // _ENTITY_H_
--- Headers/model.h
+++ Headers/model.h
@@ -1,46 +1,48 @@
 #ifndef _MODEL_H_
 #define _MODEL_H_
 
-#include "utility.h"
+#include "character.h"
 #include "entity.h"
 #include "tile.h"
+#include "utility.h"
 #include "view.h"
-#include "character.h"
 
-using std::vector;
 using std::unique_ptr;
+using std::vector;
 using State = vector<pair<Tile *, Entity *>>;
 using std::make_pair;
 
 static const Utility::Loc NEEDS_RANDOM = make_pair(-1, -1);
 
-class Model
-{
+class Model {
 public:
-    Model();
-    ~Model() = default;
+  Model();
+  ~Model() = default;
 
-    Entity *generateCharacter(Utility::Race race, Utility::Loc l = NEEDS_RANDOM);
-    Entity *generatePlayer(Utility::Race race);
-    Tile *generateTile(Utility::Terrain t);
-    void generateLayout(vector<Utility::Terrain> layout);
-    bool playerMove(Utility::Direction d);
-    bool playerUse(Utility::Direction d);
-    bool playerAttack(Utility::Direction d);
-    void setEnemyMovement(bool move) { m_move = move; }
-    void restart();
-    void quit();
-    State &state() { return m_state; }
-    void render();
-    int indiceFromLoc(Utility::Loc l);
+  Entity *generateCharacter(Utility::Race race, Utility::Loc l = NEEDS_RANDOM);
+  Entity *generatePlayer(Utility::Race race);
+  Tile *generateTile(Utility::Terrain t);
+  void generateLayout(vector<Utility::Terrain> layout);
+  bool playerMove(Utility::Direction d);
+  void enemyMove();
+  bool playerUse(Utility::Direction d);
+  bool playerAttack(Utility::Direction d);
+  void setEnemyMovement(bool move) { m_move = move; }
+  void restart();
+  void quit();
+  State &state() { return m_state; }
+  void render();
+  int indiceFromLoc(Utility::Loc l);
+
 private:
-    Entity *m_player;
-    Utility::Loc m_playerLoc;
-    bool m_move;
-    State m_state;
-    unique_ptr<View> m_view;
-    vector<unique_ptr<Entity>> m_entities;
-    vector<unique_ptr<Tile>> m_tiles;
+  void swapSpaces(pair<Tile *, Entity *> p1, pair<Tile *, Entity *> p2);
+  Entity *m_player;
+  Utility::Loc m_playerLoc;
+  bool m_move;
+  State m_state;
+  unique_ptr<View> m_view;
+  vector<unique_ptr<Entity>> m_entities;
+  vector<unique_ptr<Tile>> m_tiles;
 };
 
 #endif // _MODEL_H_
